f7a238aa37ad26351b1d7d2bd95c765cd039eca1
Regina <regina.sam@northumbria.ac.uk>
2019-03-29 15:11:03 +0000

    Regina just create a variable y

--- tutor.java
+++ tutor.java
@@ -1,6 +1,6 @@
 
 /**
- * 在这里给出对类 tutor 的描述。
+ *Regina version
  * 
  * @作者（你的名字）
  * @版本（一个版本号或者一个日期）
@@ -9,6 +9,8 @@ public class tutor
 {
     // 实例变量 - 用你自己的变量替换下面的例子
     private int x;
+     private int y;
+ 
 
     /**
      * 类 tutor 的对象的构造函数
